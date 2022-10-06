2fd370cde8156c7b08a0f94f87728789e6c687cb
stratospher <44024636+stratospher@users.noreply.github.com>
2022-10-06 11:49:28 +0530

    [test/crypto] Add HMAC-based Key Derivation Function (HKDF)
    Co-authored-by: Pieter Wuille <pieter.wuille@gmail.com>

--- test/functional/test_framework/key.py
+++ test/functional/test_framework/key.py
@@ -25,6 +25,23 @@ def TaggedHash(tag, data):
     ss += data
     return hashlib.sha256(ss).digest()
 
+# HKDF-SHA256
+def hmac_sha256(key, data):
+    """Compute HMAC-SHA256 from specified byte arrays key and data."""
+    return hmac.new(key, data, hashlib.sha256).digest()
+
+def hkdf_sha256(length, ikm, salt, info):
+    """Derive a key using HKDF-SHA256."""
+    if len(salt) == 0:
+        salt = bytes([0] * 32)
+    prk = hmac_sha256(salt, ikm)
+    t = b""
+    okm = b""
+    for i in range((length + 32 - 1) // 32):
+        t = hmac_sha256(prk, t + info + bytes([i + 1]))
+        okm += t
+    return okm[:length]
+
 class FE:
     """Objects of this class represent elements of the field GF(2**256 - 2**32 - 977).
 
@@ -637,3 +654,36 @@ class TestFrameworkKey(unittest.TestCase):
                     self.assertEqual(result, result_actual, "BIP340 test vector %i (%s): verification succeeded unexpectedly" % (i, comment))
                 num_tests += 1
         self.assertTrue(num_tests >= 15) # expect at least 15 test vectors
+
+    # TODO: Remove before push
+    def test_hkdf_sha256(self):
+        TESTS = [
+            # IKM, salt, info, L, PRK, OKM
+            ["0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b", "000102030405060708090a0b0c", "f0f1f2f3f4f5f6f7f8f9", 42,
+             "077709362c2e32df0ddc3f0dc47bba6390b6c73bb50f9c3122ec844ad7c2b3e5",
+             "3cb25f25faacd57a90434f64d0362f2a2d2d0a90cf1a5a4c5db02d56ecc4c5bf34007208d5b887185865"],
+            ["000102030405060708090a0b0c0d0e0f101112131415161718191a1b1c1d1e1f202122232425262728292a2b2c2d2e2f303132333435363738393a3b3c3d3e3f404142434445464748494a4b4c4d4e4f",
+             "606162636465666768696a6b6c6d6e6f707172737475767778797a7b7c7d7e7f808182838485868788898a8b8c8d8e8f909192939495969798999a9b9c9d9e9fa0a1a2a3a4a5a6a7a8a9aaabacadaeaf",
+             "b0b1b2b3b4b5b6b7b8b9babbbcbdbebfc0c1c2c3c4c5c6c7c8c9cacbcccdcecfd0d1d2d3d4d5d6d7d8d9dadbdcdddedfe0e1e2e3e4e5e6e7e8e9eaebecedeeeff0f1f2f3f4f5f6f7f8f9fafbfcfdfeff",
+             82,
+             "06a6b88c5853361a06104c9ceb35b45cef760014904671014a193f40c15fc244",
+             "b11e398dc80327a1c8e7f78c596a49344f012eda2d4efad8a050cc4c19afa97c59045a99cac7827271cb41c65e590e09da3275600c2f09b8367793a9aca3db71cc30c58179ec3e87c14c01d5c1f3434f1d87"],
+            ["0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b","","",42,
+             "19ef24a32c717b167f33a91d6f648bdf96596776afdb6377ac434c1c293ccb04",
+             "8da4e775a563c18f715f802a063c5a31b8a11f5c5ee1879ec3454e5f3c738d2d9d201395faa4b61a96c8"]
+        ]
+
+        for test_vector in TESTS:
+            IKM, salt, info, L, PRK, OKM = test_vector
+
+            IKM = bytearray.fromhex(IKM)
+            salt = bytearray.fromhex(salt)
+            info = bytearray.fromhex(info)
+            PRK = bytearray.fromhex(PRK)
+            OKM = bytearray.fromhex(OKM)
+
+            res_prk = hmac_sha256(salt, IKM)
+            res_okm = hkdf_sha256(L, IKM, salt, info)
+
+            assert res_prk == PRK
+            assert res_okm == OKM
