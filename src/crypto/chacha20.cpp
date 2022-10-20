bitcoin/bitcoin

Adding forward secure FSChaCha20

src/crypto/chacha20.cpp
@@ -5,8 +5,10 @@
 // Based on the public domain implementation 'merged' by D. J. Bernstein
 // See https://cr.yp.to/chacha.html.
 
-#include <crypto/common.h>
 #include <crypto/chacha20.h>
+#include <crypto/common.h>
+#include <hash.h>
+#include <support/cleanse.h>
 
 #include <string.h>
 
@@ -420,5 +422,22 @@ void ChaCha20::Crypt(const unsigned char* m, unsigned char* c, size_t bytes)
         c += 64;
         m += 64;
         prev_block_start_pos = 0;
+    }
+}
+
+void FSChaCha20::Crypt(Span<const std::byte> input, Span<std::byte> output)
+{
+    assert(input.size() == output.size());
+    c20.Crypt(reinterpret_cast<const unsigned char*>(input.data()),
+              reinterpret_cast<unsigned char*>(output.data()), input.size());
+    messages_with_key++;
+
+    if (messages_with_key % rekey_interval == 0) {
+        auto new_key = Hash(rekey_salt, key);
+        memory_cleanse(key.data(), key.size());
+        memcpy(key.data(), &new_key, FSCHACHA20_KEYLEN);
+        c20.SetKey(reinterpret_cast<unsigned char*>(key.data()), key.size());
+        rekey_counter++;
+        set_nonce();
     }
 }

src/test/fuzz/crypto_chacha20.cpp
@@ -7,6 +7,8 @@
 #include <test/fuzz/fuzz.h>
 #include <test/fuzz/util.h>
 
+#include <array>
+#include <cstddef>
 #include <cstdint>
 #include <vector>
 
@@ -41,5 +43,31 @@ FUZZ_TARGET(crypto_chacha20)
                 const std::vector<uint8_t> input = ConsumeFixedLengthByteVector(fuzzed_data_provider, output.size());
                 chacha20.Crypt(input.data(), output.data(), input.size());
             });
+    }
+}
+
+FUZZ_TARGET(crypto_fschacha20)
+{
+    FuzzedDataProvider fuzzed_data_provider{buffer.data(), buffer.size()};
+
+    auto key_vec = ConsumeFixedLengthByteVector(fuzzed_data_provider, FSCHACHA20_KEYLEN);
+    key_vec.resize(FSCHACHA20_KEYLEN);
+    auto salt_vec = ConsumeFixedLengthByteVector(fuzzed_data_provider, FSCHACHA20_KEYLEN);
+    salt_vec.resize(FSCHACHA20_KEYLEN);
+
+    std::array<std::byte, FSCHACHA20_KEYLEN> key;
+    memcpy(key.data(), key_vec.data(), FSCHACHA20_KEYLEN);
+
+    std::array<std::byte, FSCHACHA20_KEYLEN> salt;
+    memcpy(salt.data(), salt_vec.data(), FSCHACHA20_KEYLEN);
+
+    auto fsc20 = FSChaCha20{key, salt, fuzzed_data_provider.ConsumeIntegralInRange<size_t>(1, 1024)};
+
+    LIMITED_WHILE(fuzzed_data_provider.ConsumeBool(), 10000)
+    {
+        auto input = fuzzed_data_provider.ConsumeBytes<std::byte>(fuzzed_data_provider.ConsumeIntegralInRange(0, 4096));
+        std::vector<std::byte> output;
+        output.resize(input.size());
+        fsc20.Crypt(input, output);
     }
 }

stratospher/bitcoin-core-ci

src/crypto/chacha20.cpp
@@ -5,8 +5,10 @@
 // Based on the public domain implementation 'merged' by D. J. Bernstein
 // See https://cr.yp.to/chacha.html.
 
-#include <crypto/common.h>
 #include <crypto/chacha20.h>
+
+#include <crypto/common.h>
+#include <crypto/sha256.h>
 
 #include <algorithm>
 #include <string.h>
@@ -336,4 +338,28 @@ void ChaCha20::Crypt(const unsigned char* m, unsigned char* c, size_t bytes)
         }
         m_bufleft = 64 - bytes;
     }
+}
+
+void FSChaCha20::Crypt(Span<const std::byte> input, Span<std::byte> output)
+{
+    assert(input.size() == output.size());
+    c20.Crypt(reinterpret_cast<const unsigned char*>(input.data()),
+              reinterpret_cast<unsigned char*>(output.data()), input.size());
+    chunk_counter++;
+
+    if (chunk_counter % rekey_interval == 0) {
+        CommitToKey({(std::byte*)nullptr, 0});
+    }
+}
+
+void FSChaCha20::CommitToKey(const Span<const std::byte> data)
+{
+    assert(CSHA256::OUTPUT_SIZE == FSCHACHA20_KEYLEN);
+    auto hasher = rekey_hasher;
+    hasher << MakeUCharSpan(data) << MakeUCharSpan(key);
+    auto new_key = hasher.GetSHA256();
+    memory_cleanse(key.data(), key.size());
+    memcpy(key.data(), new_key.data(), FSCHACHA20_KEYLEN);
+    c20.SetKey32(reinterpret_cast<unsigned char*>(key.data()));
+    set_nonce();
 }

src/test/fuzz/crypto_chacha20.cpp
@@ -8,6 +8,8 @@
 #include <test/fuzz/util.h>
 #include <test/util/xoroshiro128plusplus.h>
 
+#include <array>
+#include <cstddef>
 #include <cstdint>
 #include <vector>
 
@@ -149,4 +151,30 @@ FUZZ_TARGET(chacha20_split_keystream)
 {
     FuzzedDataProvider provider{buffer.data(), buffer.size()};
     ChaCha20SplitFuzz<false>(provider);
+}
+
+FUZZ_TARGET(crypto_fschacha20)
+{
+    FuzzedDataProvider fuzzed_data_provider{buffer.data(), buffer.size()};
+
+    auto key_vec = ConsumeFixedLengthByteVector(fuzzed_data_provider, FSCHACHA20_KEYLEN);
+    key_vec.resize(FSCHACHA20_KEYLEN);
+    auto salt_vec = ConsumeFixedLengthByteVector(fuzzed_data_provider, FSCHACHA20_KEYLEN);
+    salt_vec.resize(FSCHACHA20_KEYLEN);
+
+    std::array<std::byte, FSCHACHA20_KEYLEN> key;
+    memcpy(key.data(), key_vec.data(), FSCHACHA20_KEYLEN);
+
+    std::array<std::byte, FSCHACHA20_REKEY_SALT_LEN> salt;
+    memcpy(salt.data(), salt_vec.data(), FSCHACHA20_REKEY_SALT_LEN);
+
+    auto fsc20 = FSChaCha20{key, salt, fuzzed_data_provider.ConsumeIntegralInRange<size_t>(1, 1024)};
+
+    LIMITED_WHILE(fuzzed_data_provider.ConsumeBool(), 10000)
+    {
+        auto input = fuzzed_data_provider.ConsumeBytes<std::byte>(fuzzed_data_provider.ConsumeIntegralInRange(0, 4096));
+        std::vector<std::byte> output;
+        output.resize(input.size());
+        fsc20.Crypt(input, output);
+    }
 }